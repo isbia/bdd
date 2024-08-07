ad174f98fde1a35b04d19e0246ace1c05ce68863
Ana Beatriz <ana.melo1@aluno.ifsp.edu.br>
2024-08-07 10:41:23 -0300

    semana 1 - inserts

---
+++ 2024-08-07/insert-user.js
@@ -0,0 +1,13 @@
+// inserrt-user.js
+
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const data = { name: 'Hello, Prisma!' };
+    await prisma.user.create({ data });
+    console.log("Cadastro realizado com sucesso!");
+}
+
+main();
